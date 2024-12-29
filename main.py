db76907220ec7f4484e0ff64b905070f3f1d12e8
Maicon Cunha <maicoboeira@hotmail.com>
2024-12-29 16:06:36 -0300

    adicionando exercicio 04

--- main.py
+++ main.py
@@ -42,10 +42,10 @@
 # como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
 # escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
 
-log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
+# log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
 
-if log['level'] == 'ERROR':
-    print(log['message'])
+# if log['level'] == 'ERROR':
+#     print(log['message'])
 
 
 ### Exercício 4: Validação de Dados de Entrada
@@ -53,6 +53,19 @@ if log['level'] == 'ERROR':
 # você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha 
 # fornecido um email válido. Escreva um programa que valide essas condições 
 # e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+
+idade = 42  # Exemplo de valor, substitua com input do usuário se necessário
+email = "usuario@exemplo.com"  # Exemplo de valor, substitua com input do usuário se necessário
+
+if not 18 <= idade <= 65:
+    print("Idade fora do intervalo permitido")
+elif "@" not in email or "." not in email:
+    print("Email inválido")
+else:
+    print("Dados de usuário válidos")
+
+
+
 
 ### Exercício 5: Detecção de Anomalias em Dados de Transações
 # Você está trabalhando em um sistema de detecção de fraude e precisa identificar 
